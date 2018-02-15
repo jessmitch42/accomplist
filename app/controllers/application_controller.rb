70f06cf3696ff021dc6b9c756b53dd90b2ae1be7
jessmitch42 <jessicamitch42@gmail.com>
2018-02-15 12:46:56 -0500

    welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user #move to helper
 
-  def current_user
-    current_user
-  end
+  
 end
